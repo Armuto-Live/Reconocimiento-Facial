ffecc462afa02578aeb8229421f450db954e13e9
Armuto-Live <luisarmuto3@gmail.com>
2021-11-14 17:29:44 -0500

    Ordenando Puntos

--- encontrarContorno/3.ContandoMonedasCamara/ContadorMonedasCamara.py
+++ encontrarContorno/3.ContandoMonedasCamara/ContadorMonedasCamara.py
@@ -1,19 +1,16 @@
 import cv2
+import numpy
+from numpy.lib.function_base import iterable
 
-urlCamara='http://192.168.1.65:4747/video'
+def OrdenarPuntos(puntos):
+    numero_puntos=numpy.concatenate(puntos[0],puntos[1],puntos[2],puntos[3]).tolist()
+    
+    coordenada_y=sorted(numero_puntos,key=lambda numero_puntos:numero_puntos[1])
 
-capturarVideo=cv2.VideoCapture(urlCamara)
+    coordenada_x1=coordenada_y[0:2]
+    coordenada_x1=sorted(coordenada_x1,key=lambda coordenada_X1:coordenada_x1[0])
 
+    coordenada_x2=coordenada_y[2:4]
+    coordenada_x2=sorted(coordenada_x2,key=lambda coordenada_x2:coordenada_x2[0])
 
-if not capturarVideo.isOpened():
-    print("No se encontró la cámara :)")
-    exit()
-while True:
-    tipoCamara,camara=capturarVideo.read()
-    escalaGris=cv2.cvtColor(camara,cv2.COLOR_BGR2GRAY)
-    cv2.imshow("Camara en vivo",escalaGris)
-
-    if cv2.waitKey(1)==ord("q"):
-        break
-capturarVideo.release()
-cv2.destroyAllWindows()
+    return [coordenada_x1[0],coordenada_x1[1],coordenada_x2[0],coordenada_x2[1]]
---
+++ encontrarContorno/3.ContandoMonedasCamara/MostrarCamara.py
@@ -0,0 +1,19 @@
+import cv2
+
+urlCamara='http://192.168.1.65:4747/video'
+
+capturarVideo=cv2.VideoCapture(urlCamara)
+
+
+if not capturarVideo.isOpened():
+    print("No se encontró la cámara :)")
+    exit()
+while True:
+    tipoCamara,camara=capturarVideo.read()
+    escalaGris=cv2.cvtColor(camara,cv2.COLOR_BGR2GRAY)
+    cv2.imshow("Camara en vivo",escalaGris)
+
+    if cv2.waitKey(1)==ord("q"):
+        break
+capturarVideo.release()
+cv2.destroyAllWindows()
